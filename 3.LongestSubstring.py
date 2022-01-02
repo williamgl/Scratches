ec8e9944fc00340ed0b6a7e9b7d0e45eb9ddb0c4
Gan Li <billgan.lee@gmail.com>
2022-01-01 22:04:12 -0800

    Leetcode 3 - Time Limit Exceed

---
+++ 3.LongestSubstring.py
@@ -0,0 +1,40 @@
+# Time Limit Exceed
+"""
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        length = len(s)
+        answer = 0
+
+        if length == 1:
+            return 1
+
+        for i in range(0, length - 1):
+            for j in range(i + 1, length + 1):
+                if j - i > 90:
+                    pass
+                else:
+                    string = s[i:j]
+                    if self.check(string):
+                        answer = max(answer, len(string))
+
+        return answer
+
+    def check(self, s):
+        length = len(s)
+        for i in range(0, length - 1):
+            if s[i] in s[i + 1:]:
+                return False
+        return True
+"""
+
+# Best Solution
+res = 0
+word = ""
+for c in s:
+    if c not in word:
+        word += c
+        if len(word) > res:
+            res = len(word)
+    else:
+        word = word[word.find(c) + 1:] + c
+return res
