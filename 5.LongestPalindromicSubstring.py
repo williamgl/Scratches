cb162daad2b091e715742ccc4a856611671d0f6e
Gan Li <billgan.lee@gmail.com>
2022-01-16 12:10:51 -0800

    LeetCode 5 - Manacher's Algorithm

--- 5.LongestPalindromicSubstring.py
+++ 5.LongestPalindromicSubstring.py
@@ -14,3 +14,18 @@ class Solution:
         return max([i for i in l if i[::-1]==i],key=len)
 
 """
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        def getPalindrome(string, left, right):
+            while left >= 0 and right < len(string) and string[left] == string[right]:
+                left -= 1
+                right += 1
+            return string[left+ 1: right]
+        ans = ''
+        for i in range(len(s)):
+            # odd numbers
+            ans1 = getPalindrome(s, i, i)
+            # even numbers
+            ans2 = getPalindrome(s, i, i + 1)
+            ans = max([ans, ans1, ans2], key=lambda x: len(x))
+        return ans
