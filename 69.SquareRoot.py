e3a15d4c3b5bbc5a56af58b11e228d0ad542a365
Gan Li <billgan.lee@gmail.com>
2022-01-16 16:59:16 -0800

    LeetCode 69

---
+++ 69.SquareRoot.py
@@ -0,0 +1,10 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x < 2: return x
+
+        x0 = x
+        x1 = (x0 + x / x0) / 2
+        while abs(x0 - x1) >= 1:
+            x0 = x1
+            x1 = (x0 + x / x0) / 2
+        return int(x1)
